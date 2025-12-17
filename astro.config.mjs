253386d3bc4bdc8a8c414f5f33076d87523af9a7
hardik-novus <hardikpatel.nl@gmail.com>
2025-12-17 13:22:41 +0530

    - setup project for landing page only

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://vividinktattoos.co.uk',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    })
+  ],
+  
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  
+  compressHTML: true,
+});
